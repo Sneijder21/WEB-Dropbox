04a60a8e1cd5d876b2bb444d2c84e862c4166d70
neyser <neyser36@gmail.com>
2019-06-07 23:11:25 -0500

    Creacion de la migracion de manejo de archivos

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -2,15 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\File;
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
 class FilesController extends Controller
 {
     public function index()
     {
         return view('Backend.Files');
     }
-    public function store()
-    {
 
+    public function store(Request $request)
+    {
+        //$request->file('file');
+        // $nombre=$request->file->getClientOriginalName();
+        // $request->file->storeAs('public',$nombre);
+
+        // $file=new File;
+        // $file->name=$nombre;
+        // $file->save();
+        // return "el archivo se subio exitosamente";
+
+        $request->file('file');
+        return $request->file->storeAs('public', $request->file->getClientOriginalName());
+    }
+
+    public function show()
+    {
+        return Storage::files('public');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -9,18 +9,21 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
 /* Rutas agrupada */
-Route::prefix('panel')->group(function(){
-    Route::view('/','Backend.Dashboard')->name('admin');
-    Route::view('users','Backend.Users')->name('admin.user');
+Route::prefix('panel')->group(function () {
+
+    Route::view('/', 'Backend.Dashboard')->name('admin');
+    Route::view('users', 'Backend.Users')->name('admin.user');
     
-    Route::get('upload_files','FilesController@index')->name('admin.upload.index');
-    Route::get('upload_files/store','FilesController@store')->name('admin.upload.store');
+    Route::get('upload_files', 'FilesController@index')->name('admin.files.index');
+    Route::post('archivo-subido', 'FilesController@store')->name('admin.files.store');
+    Route::get('ver-archivo-subido', 'FilesController@show')->name('admin.files.show');
+    /*Route::post('upload_files/store','FilesController@store')->name('admin.upload.store');*/
 
 });
 
@@ -30,9 +33,9 @@ Route::view('admin/users','Backend.Users');
 Route::view('admin/files','Backend.Files'); */
 
 /* Ruta con manejo de funciones y restricciones */
-Route::get('/user/{username?}',function($username='Invitado'){
+Route::get('/user/{username?}', function ($username = 'Invitado') {
     return $username;
-})->where('username','[A-Za-z]+');
+})->where('username', '[A-Za-z]+');
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
